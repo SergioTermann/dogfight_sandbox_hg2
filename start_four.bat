2e1f2d6ccfb3902d2a5bc14105f4a945c3edbc47
SergioTermann <1064756429@qq.com>
2025-12-18 07:20:08 +0800

    111

--- start_four.bat
+++ start_four.bat
@@ -1,8 +1,9 @@
 cd source
-start cmd /k "..\bin\python\python.exe main.py --port 57805"
-start cmd /k "..\bin\python\python.exe main.py --port 57806"
-start cmd /k "..\bin\python\python.exe main.py --port 57807"
-start cmd /k "..\bin\python\python.exe main.py --port 57808"
+REM 使用系统 Python（已安装 jsbsim）
+start cmd /k "python main.py --port 57805"
+start cmd /k "python main.py --port 57806"
+start cmd /k "python main.py --port 57807"
+start cmd /k "python main.py --port 57808"
 @REM ..\bin\python\python.exe main.py --port 57806
 @REM ..\bin\python\python.exe main.py --port 57806
 pause
