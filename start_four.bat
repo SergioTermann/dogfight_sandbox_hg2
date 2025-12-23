77b61c12c187e97f3222fb685e8cc261d4c424fd
SergioTermann <1064756429@qq.com>
2025-12-23 17:17:09 +0800

    Remove obsolete batch files and documentation related to simulation acceleration system.

--- start_four.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd source
-start cmd /k "conda activate bh && python main.py --port 57805"
-start cmd /k "conda activate bh && python main.py --port 57806"
-start cmd /k "conda activate bh && python main.py --port 57807"
-start cmd /k "conda activate bh && python main.py --port 57808"
-@REM ..\bin\python\python.exe main.py --port 57806
-@REM ..\bin\python\python.exe main.py --port 57806
-pause
--- start_total.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd source
-start cmd /k "conda activate bh && python main.py --port 57805"
-start cmd /k "conda activate bh && python main.py --port 57806"
-pause
